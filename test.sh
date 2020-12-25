9357e35e8d52329e61ba761c2f62ea6359217d21
Nick <nick@kousu.ca>
2020-12-25 02:52:50 -0500

    Demonstrate hanging on Github's macOS VMs

---
+++ test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export PS4='+ ' # make Travis/Github closer; this is used by set -x
+
+exec 2>&1       # Actions seems to not be great about syncronizing the output from stderr and stdout; travis is
+                # hopefully forcing both to stdout will make the output consistent
+
+set -x
+
+echo
+echo "Context:"
+echo "--------"
+uname -a
+whoami
+set
+
+echo
+echo "Test:"
+echo "-----"
+echo "How many times does this run?"
+yes | head -n 7
+echo "Great, I see you're finished."
