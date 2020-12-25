b2b8e6aaeebaa0f1035279f6cee4403d0ba42394
Nick <nguenthe@uwaterloo.ca>
2020-12-25 18:22:07 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,6 +5,15 @@ export PS4='+ ' # make Travis/Github closer; this is used by set -x
 exec 2>&1       # Actions seems to not be great about syncronizing the output from stderr and stdout; travis is
                 # hopefully forcing both to stdout will make the output consistent
 
+# generate an infinite list (cribbed from https://docs.python.org/3/library/itertools.html#itertools.count)
+count() {
+  i=0
+  while true; do
+    i=$(($i + 1))
+    echo $i
+  done 
+}
+
 set -x
 
 echo
@@ -19,7 +28,7 @@ echo "Test:"
 echo "-----"
 echo "How many times does this run?"
 #yes | head -n 7
-seq | while read i; do
+count | while read i; do
   echo $i;
   if [ $i -gt 7 ]; then
     break;
