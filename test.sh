2fbb7f9ee8c818afbb751f1a8d0cd0afe3ba3439
Nick <nguenthe@uwaterloo.ca>
2020-12-25 18:23:59 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -7,6 +7,7 @@ exec 2>&1       # Actions seems to not be great about syncronizing the output fr
 
 # generate an infinite list (cribbed from https://docs.python.org/3/library/itertools.html#itertools.count)
 count() {
+  set +x
   i=0
   while true; do
     i=$(($i + 1))
